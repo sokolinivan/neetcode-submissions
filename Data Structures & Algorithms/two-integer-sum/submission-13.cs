34de13e49aa2cc08ab2f558be9f8de0f7fb0e234
sokolinivan <sokolin.id@gmail.com>
2026-08-11 10:48:51 +0300

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (var i = 0; i < nums.Length; i++) {
+            for (var j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i,j};
+                }
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+}
